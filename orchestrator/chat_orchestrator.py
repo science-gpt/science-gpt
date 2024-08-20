270009c5b819a2eeedcbe7b293fe2f61669e66a1
Carter Demars <demarscarter@yahoo.ca>
2024-08-20 18:02:07 -0400

    orchestrator skeleton

--- orchestrator/chat_orchestrator.py
+++ orchestrator/chat_orchestrator.py
@@ -1,24 +1,39 @@
+import os
+
+import toml
+
+from models.models import OpenAIChatModel
 from orchestrator.config import SystemConfig
 from orchestrator.utils import load_config
 from retrieval import prompts
-#from langchain_community.vectorstores.chroma import Chroma
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableBranch
+
 
 class ChatOrchestrator:
     def __init__(self) -> None:
-        self.config: SystemConfig = load_config(config_name="system_config", config_dir="./configs")
-    
-    def triage_query(query: str) -> str:
+        self.config: SystemConfig = load_config(
+            config_name="system_config", config_dir=f"{os.getcwd()}/configs"
+        )
+        self.load_secrets()
+
+        self.llm = OpenAIChatModel(self.config)
+
+    def load_secrets(self):
         """
-        Given a user query, the orchestrator detects user intent and leverages 
+        Load secrets from toml file into config object.
+        """
+        secrets = toml.load("secrets.toml")
+
+        # hardcoded to use gpt3.5 for now
+        self.config.model_auth["version"] = secrets["gpt35-api"]["api_version"]
+        self.config.model_auth["api_key"] = secrets["gpt35-api"]["api_key"]
+        self.config.model_auth["url"] = secrets["gpt35-api"]["azure_endpoint"]
+
+    def triage_query(self, query: str) -> str:
+        """
+        Given a user query, the orchestrator detects user intent and leverages
         appropriate agents to provide a response.
         """
 
-        return "hello"
+        response = self.llm(query)
 
-
+        return response.content
--- orchestrator/config.py
+++ orchestrator/config.py
@@ -1,13 +1,5 @@
 from pydantic.dataclasses import dataclass
 
-# @dataclass
-# class ModelConfig:
-#     model_name: str
-
-
-# @dataclass
-# class OpenAIModelConfig(ModelConfig):
-#     model_params: dict
 
 @dataclass
 class SystemConfig:
