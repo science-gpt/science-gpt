813467ac226a5971f09ce62684eb6b92525a6cf4
Areel <areel15511@gmail.com>
2024-09-08 17:48:44 -0400

    created factor functinos inside of databroker

--- src/data_broker/data_broker.py
+++ src/data_broker/data_broker.py
@@ -1,7 +1,8 @@
 from typing import List
-
-from ingestion.chunking import CustomTextSplitter
-from ingestion.extraction import PDFExtract, Text
+from ingestion.chunking import CustomTextSplitter, Chunker
+from ingestion.extraction import PDFExtract, Extractor
+from ingestion.embedding import Embedder
+from ingestion.vector_db import VectorDB
 from ingestion.raw_data import RawData
 from orchestrator.config import SystemConfig
 
@@ -30,47 +31,6 @@ class DataBroker:
         """
         self.config = config
 
-    def _extract(self, data: RawData) -> Text:
-        """
-        Extract text from the given raw data.
-
-        :param data: The raw data object to extract text from
-        :return: The extracted text
-        :raises ValueError: If the data type is not recognized or supported
-        """
-        if data.data_type == "pdf":
-            extractor = PDFExtract()
-        else:
-            # TODO handle case when datatype is unrecognized
-            raise ValueError
-
-        return extractor(data=data)
-
-    def _chunk(self, text: Text) -> List[Chunk]:
-        """
-        Split the input text into chunks using the configured chunking method.
-
-        :param text: The input text to be chunked
-        :return: A list of text chunks
-        :raises ValueError: If the configured chunking method is not supported
-        """
-        if self.config.chunking_method == "custom-text-splitter":
-            chunker = CustomTextSplitter()
-        else:
-            # TODO unsupported chunking method
-            raise ValueError
-
-        return chunker(text=text)
-
-    def _embed(self, chunks: List[Chunk]):
-        """
-        Embed the given chunks using the configured embedding model.
-
-        :param chunks: A list of text chunks to be embedded
-        :return: A list of embedded vectors
-        """
-        raise NotImplementedError("Embedding method not yet implemented")
-
     def insert(self, data: RawData) -> None:
         """
         Process and insert the given raw data into the vector store.
@@ -82,10 +42,53 @@ class DataBroker:
         :raises ValueError: If any step in the process fails due to unsupported
             data types or methods
         """
-        text = self._extract(data=data)
-        chunks = self._chunk(text=text)
-        vectors = self._embed(chunks=chunks)
+        extractor = self._create_extractor()
+        chunker = self._create_chunker()
+        embedder = self._create_embedder()
+        vector_store = self._create_vector_store()
 
-        # embed
-        # insert into vector store
-        # return confirmation
+        text = extractor(data)
+        chunks = chunker(text)
+        vectors = embedder(chunks)
+        vector_store.insert(vectors)
+
+    def _create_extractor(self, data_type: str) -> Extractor:
+        """
+        Creates an extractor based on the given data type.
+
+        :param data_type: The type of data to extract
+        :return: A function that extracts text from the given data
+        :raises ValueError: If the data type is not recognized or supported
+        """
+        # Implement based on config
+        raise NotImplementedError("Embedding creation not yet implemented")
+
+    def _create_chunker(self) -> Chunker:
+        """
+        Creates a chunker based on the configured chunking method.
+
+        :return: A function that splits the input text into chunks
+        :raises ValueError: If the configured chunking method is not supported
+        """
+        # Implement based on config
+        raise NotImplementedError("Embedding creation not yet implemented")
+
+    def _create_embedder(self) -> Embedder:
+        """
+        Creates an embedder based on the configured embedding model.
+
+        :return: A function that embeds the given chunks
+        :raises NotImplementedError: If the embedding creation is not yet implemented
+        """
+        # Implement based on config
+        raise NotImplementedError("Embedding creation not yet implemented")
+
+    def _create_vector_store(self) -> VectorDB:
+        """
+        Creates a vector store based on the configured vector store.
+
+        :return: A function that inserts vectors into the vector store
+        :raises NotImplementedError: If the vector store creation is not yet implemented
+        """
+        # Implement based on config
+        raise NotImplementedError("Vector store creation not yet implemented")
