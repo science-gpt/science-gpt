bdc72b4d6f3bd839e7b699ef75910f322694f5ce
Areel Khan <areel15511@gmail.com>
2024-09-10 16:44:28 -0400

    added docstrings to embedding

--- src/ingestion/embedding.py
+++ src/ingestion/embedding.py
@@ -62,10 +62,26 @@ class Embedder(ABC):
 
 
 class HuggingFaceSentenceTransformerEmbedder(Embedder):
+    """
+    An embedder that uses HuggingFace's Sentence Transformer models to create embeddings.
+    """
+
     def __init__(self, model_name: str = "sentence-transformers/all-mpnet-base-v2"):
+        """
+        Initialize the HuggingFaceSentenceTransformerEmbedder.
+
+        :param model_name: The name of the Sentence Transformer model to use.
+                           Defaults to "sentence-transformers/all-mpnet-base-v2".
+        """
         self.model = HuggingFaceEmbeddings(model_name=model_name)
 
     def __call__(self, chunks: List[Chunk]) -> List[Embedding]:
+        """
+        Embed a list of text chunks into vectors using the Sentence Transformer model.
+
+        :param chunks: List of Chunk objects to be embedded.
+        :return: A list of Embedding objects containing the embedded vectors and metadata.
+        """
         embeddings = []
         for chunk in chunks:
             vector = self.model.embed_query(chunk.text)
