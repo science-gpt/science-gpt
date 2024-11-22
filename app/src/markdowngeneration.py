24b03c4c6026f7705fc861183f8bf9c875cdb092
spencer-crook <spencer.ronald.crook@gmail.com>
2024-11-22 17:39:53 +0000

    got rid of uncessary scripts and output files

--- app/src/markdowngeneration.py
+++
@@ -1,6 +0,0 @@
-# convert the document to markdown
-import pymupdf4llm
-md_text = pymupdf4llm.to_markdown("/home/science-gpt/science-gpt/app/data/Tetraconazole PRD2012-29.pdf", table_strategy='lines')
-# Write the text to some file in UTF8-encoding
-import pathlib
-pathlib.Path("output.md").write_bytes(md_text.encode())
