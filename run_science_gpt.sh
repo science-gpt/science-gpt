b24edab84e1b37b6b1ddf26906fe09e8b76f9a05
Areel Khan <areel15511@gmail.com>
2024-11-21 21:52:20 -0500

    added dev mode to run script

--- run_science_gpt.sh
+++ run_science_gpt.sh
@@ -1,2 +1,7 @@
 mkdir ./app/data # dir must exist for dockerfile
-docker compose -f docker-compose.yaml -f docker-compose-milvus.yaml up --pull always
+
+if [ "$1" = "--dev" ]; then
+    DEV_MODE=true docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always
+else
+    docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always
+fi
