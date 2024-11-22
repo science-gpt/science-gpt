89b035a6aae8766887b7a5c3929f9c7439a58b48
Areel Khan <areel15511@gmail.com>
2024-11-21 19:38:02 -0500

    added dev mode that turns off auth

--- app/auth.py
+++ app/auth.py
@@ -45,45 +45,43 @@ else:
         config["cookie"]["expiry_days"],
     )
 
+    def signup():
+        try:
+            (
+                email_of_registered_user,
+                username_of_registered_user,
+                name_of_registered_user,
+            ) = authenticator.register_user(
+                pre_authorized=config["pre-authorized"]["emails"]
+            )
+            if email_of_registered_user:
+                st.success("User registered successfully")
+            with open("./src/configs/user_config.yaml", "w") as file:
+                yaml.dump(config, file, default_flow_style=False)
+        except Exception as e:
+            st.error(e)
 
-def signup():
-    try:
-        (
-            email_of_registered_user,
-            username_of_registered_user,
-            name_of_registered_user,
-        ) = authenticator.register_user(
-            pre_authorized=config["pre-authorized"]["emails"]
-        )
-        if email_of_registered_user:
-            st.success("User registered successfully")
-        with open("./src/configs/user_config.yaml", "w") as file:
-            yaml.dump(config, file, default_flow_style=False)
-    except Exception as e:
-        st.error(e)
+    col1, col2 = st.columns(2)
 
+    with col1:
+        try:
+            authenticator.login()
+            if st.session_state["authentication_status"] is False:
+                st.error("Username/password is incorrect")
+                st.session_state.logged_in = False
+            elif st.session_state["authentication_status"] is None:
+                st.warning("Please enter your username and password")
+                st.session_state.logged_in = False
+        except Exception as e:
+            st.error(e)
 
-col1, col2 = st.columns(2)
+    if st.session_state["authentication_status"]:
+        authenticator.logout(location="sidebar")
+        if not st.session_state.logged_in:
+            st.toast(f'Welcome *{st.session_state["name"]}*', icon="👋")
+            st.session_state.logged_in = True
+        sciencegpt()
 
-with col1:
-    try:
-        authenticator.login()
-        if st.session_state["authentication_status"] is False:
-            st.error("Username/password is incorrect")
-            st.session_state.logged_in = False
-        elif st.session_state["authentication_status"] is None:
-            st.warning("Please enter your username and password")
-            st.session_state.logged_in = False
-    except Exception as e:
-        st.error(e)
-
-if st.session_state["authentication_status"]:
-    authenticator.logout(location="sidebar")
-    if not st.session_state.logged_in:
-        st.toast(f'Welcome *{st.session_state["name"]}*', icon="👋")
-        st.session_state.logged_in = True
-    sciencegpt()
-
-else:
-    with col2:
-        signup()
+    else:
+        with col2:
+            signup()
