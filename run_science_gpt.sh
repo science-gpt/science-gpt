6b7e55280a207a35395cb06615c633cafc4aa34b
bing1100 <bingxuhu@gmail.com>
2025-01-31 21:31:07 -0500

    no-gpu flag

--- run_science_gpt.sh
+++ run_science_gpt.sh
@@ -3,6 +3,7 @@ mkdir ./app/data # dir must exist for dockerfile
 DEV_MODE=false
 BUILD=false
 UPDATE_DEPS=false
+NO_GPU=false
 
 # Parse arguments
 for arg in "$@"
@@ -17,15 +18,18 @@ do
         --update-deps)
         UPDATE_DEPS=true
         ;;
+        --no-gpu)
+        NO_GPU=true
+        ;;
     esac
 done
 
 if [ "$DEV_MODE" = true ] && [ "$BUILD" = true ]; then
-    DEV_MODE=true UPDATE_DEPS=$UPDATE_DEPS docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always --build
+    GPU=$NO_GPU DEV_MODE=true UPDATE_DEPS=$UPDATE_DEPS docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always --build
 elif [ "$DEV_MODE" = true ]; then
-    DEV_MODE=true docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always
+    GPU=$NO_GPU DEV_MODE=true docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always
 elif [ "$BUILD" = true ]; then
-    UPDATE_DEPS=$UPDATE_DEPS docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always --build
+    GPU=$NO_GPU UPDATE_DEPS=$UPDATE_DEPS docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always --build
 else
-    docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always
+    GPU=$NO_GPU docker compose -f docker-compose-sciencegpt.yaml -f docker-compose-milvus.yaml up --pull always
 fi
