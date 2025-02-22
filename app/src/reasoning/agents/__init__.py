11b2ebe9c81452545fda43e908927ea65f7a15c8
bing1100 <bingxuhu@gmail.com>
2025-02-21 19:50:36 -0500

    local database / search

--- app/src/reasoning/agents/__init__.py
+++ app/src/reasoning/agents/__init__.py
@@ -2,7 +2,14 @@ from .base import BaseAgent
 from .io import AgentFinish, AgentOutput, AgentType, BaseScratchPad
 from .langchain_based import LangchainAgent
 from .rewoo.agent import RewooAgent
-from .tools import BaseTool, ComponentTool, GoogleSearchTool, LLMTool, WikipediaTool
+from .tools import (
+    BaseTool,
+    ComponentTool,
+    GoogleSearchTool,
+    LLMTool,
+    LocalSearchTool,
+    WikipediaTool,
+)
 
 __all__ = [
     # agent
@@ -15,6 +22,7 @@ __all__ = [
     "GoogleSearchTool",
     "WikipediaTool",
     "LLMTool",
+    "LocalSearchTool",
     # io
     "AgentType",
     "AgentOutput",
